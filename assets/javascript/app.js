08b3a1242c71cca97ab7877186efd023f703fe61
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-08-21 08:22:17 -0400

    complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,12 @@ $(function(){ // .ready() handler
 	const APIKEY = "4b2610846efe448fa64e5bbfd561d517";
 
 	// Topics array
-	let topic = ["high five", "thumbs up", "applause", "shrug", "wink", "smh", "hugs", "bye", "hi", "dance", "sleepy"];
+	let topic = ["high five", "thumbs up", "clap", "shrug", "wink", "smh", "hugs", "bye", "hi", "dance", "sleepy"];
 
-	// Create and display buttons on HTML
+	// Create and display buttons in alphabetical order on HTML
 	function renderButtons() {
 		$("#btn-list").empty();
+		topic = topic.sort();
 		for(var i = 0; i < topic.length; i++){
 		  var btn = $("<button>");
 		  btn.addClass("btn btn-default btn-topic");
@@ -21,9 +22,12 @@ $(function(){ // .ready() handler
 	// Updates HTML with the gifs of the selected topic
 	function displayGifs(){
 		$("#gif-content").empty();
+		
 		// update search term with clicked button value
 		let searchTerm = $(this).val();
 		let queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+APIKEY+"&q="+searchTerm+"&limit=10";
+	    
+	    // Call to the API to get content
 	    $.ajax({
 	      url: queryURL,
 	      method: "GET"
@@ -33,8 +37,9 @@ $(function(){ // .ready() handler
 	    	// Loop through results array and add each gif and rating to HTML
 	    	for (var i = 0; i < results.length; i++){
 		        var gifDiv = $("<div>");
-		        gifDiv.addClass("gif-div");
+		        gifDiv.addClass("thumbnail");
 		        var p = $("<p>");
+		        p.addClass("caption");
 		        p.text("Rating: " + results[i].rating);
 		        var gifImage = $("<img>");
 		        gifImage.addClass("gif");
@@ -44,9 +49,9 @@ $(function(){ // .ready() handler
 		        gifDiv.append(p);
 		        $("#gif-content").prepend(gifDiv);
 	    	};
-
 	    });
 	}
+
 	// Toggles the source of the image between still and animated
 	function togglePlay(){
 		var temp = $(this).attr("src");
@@ -57,15 +62,13 @@ $(function(){ // .ready() handler
 	// Form input to add user-input topic to array
 	$("#add-topic").on("click", function(event){
 		event.preventDefault();
-		// Here we grab the text from the input box and trim off any white space
 		let newTopic = $("#topic-input").val().trim();
 		topic.push(newTopic);
 		// remake buttons based on form input
 		renderButtons();
 	});
 
-
-	// onClick functionality for topic buttons
+	// onClick functionality for topic buttons and gifs
 	$(document).on("click", ".btn-topic", displayGifs);
 	$(document).on("click", ".gif", togglePlay);
 
